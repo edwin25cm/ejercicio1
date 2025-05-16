11dd74b760d5eb7c9575cf4cf698e573c8caf5db
edwin <edwinmachucachavez@gmail.com>
2025-05-15 21:33:35 -0300

    ejercicio1

---
+++ codigo.js
@@ -0,0 +1,6 @@
+ var v1 = document .getElementById("variable");
+  var nombre = "edwin"
+  var apellido = "machuca chavez"
+  var resultado1 = nombre + ' ' + apellido 
+
+ v1.innerText +=  ' ' + resultado1  ;
