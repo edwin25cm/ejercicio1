fbad7f6ced5b8fefdaf6499aa94a06559ac979d9
edwin <edwinmachucachavez@gmail.com>
2025-05-20 21:09:27 -0300

    variables en javascript

--- codigo.js
+++ codigo.js
@@ -1,6 +1,21 @@
- var v1 = document .getElementById("variable");
-  var nombre = "edwin"
-  var apellido = "machuca chavez"
-  var resultado1 = nombre + ' ' + apellido 
+ var v1 = document .getElementById("var-Texto");
+ var v2 = document .getElementById("var-Entera");
+ var v3 = document .getElementById("var-Boleano");
 
- v1.innerText +=  ' ' + resultado1  ;
+ //tipo string (texto)
+ var Nombre = "edwin";
+ //tipo number (numero)
+ var Edad = 50.2;
+ //tipo boleano (booleano)
+ var Vive = true; 
+
+ var vector = ['juan', 'pedro', 'manuel',true,13.37]
+
+//operaciones
+ var opNombre = 'Colegio' + ' ET Nro 12';
+ var opEdad
+ var opVive
+//muestro las variables en los respectivos elementos
+ v1.innerText +=  ' ' + opNombre ;
+ v1.innerText +=  ' ' + opEdad ;
+ v1.innerText +=  ' ' + vector ;
